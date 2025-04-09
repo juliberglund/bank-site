152bf5a2a1fcd3c433d3d4e371eb88f09a0d7838
Julia Berglund <jullano9@hotmail.com>
2025-04-09 13:01:40 +0200

    dosnt work

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,9 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}", // Adjust to match your project structure
+    "./app/**/*.{js,ts,jsx,tsx}", // Adjust according to your project structure
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
