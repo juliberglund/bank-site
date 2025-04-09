04f78338130cec687547cdbc1911c3ad14cba383
Julia Berglund <jullano9@hotmail.com>
2025-04-09 09:46:31 +0200

    test1

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,14 @@
+import path from "path";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(), // Adjust 'src' to your project's actual directory
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
